9f99841898b2c8aa58e0c2baf32fbc042e77fb65
Andres Coto <andres.coto@multimoney.com>
2025-09-26 12:41:37 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MultimoneySystem from './MultimoneySystem.jsx';
+import './styles/multimoney.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MultimoneySystem />
+    </div>
+  );
+}
+
+export default App;
